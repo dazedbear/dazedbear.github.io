1b2da418594c677d88cfdecebf0e73d2173670cf
dazedbear <angus912584@gmail.com>
2019-05-18 22:48:24 +0800

    build: add gh-page ci

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -30,11 +30,13 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'dazedbear',
+  projectName: 'dazedbear.github.io',
   organizationName: 'dazedbear',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
+
+  cname: 'dazedbear.pro',
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
