4738c6417c612dd13ba7ed31fef6d661a65a5bfd
dazedbear <angus912584@gmail.com>
2021-03-02 00:22:49 +0800

    build: remove normailze.css
    since we use tailwind css which includes base style sheet Preflight,
    we no longer need this reset styles.
    
    see https://tailwindcss.com/docs/preflight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
-import 'normalize.css'
 import '../styles/global.css'
+import '../styles/notion.css'
+import '../styles/homepage.css' // TODO: refactor css
 import 'react-notion-x/src/styles.css'
 import 'katex/dist/katex.css'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'rc-dropdown/assets/index.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all'
-import '../styles/notion.css'
-import '../styles/homepage.css' // TODO: refactor css
 import Header from '../components/header'
 import Footer from '../components/footer'
 import sharedStyles from '../styles/shared.module.css'
