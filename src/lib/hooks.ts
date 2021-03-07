410b7161ab915f05ec5ab49431cffd038b7c8df1
dazedbear <angus912584@gmail.com>
2021-03-08 00:40:36 +0800

    fix: remove fallback image check since the fallback image load check causes race condition when handling broken images, just remove it

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,39 +1,23 @@
 import { useEffect } from 'react'
 
-// FIXME: this approach has race condition during build time since some images are already borken before this effect executed.
 export const useBrokenImageHandler = ({ selector, fallbackImageUrl }) => {
   if (!selector) {
     return
   }
   useEffect(() => {
     const images = document.querySelectorAll(selector)
-
     const loadFallbackImageHandler = e => {
       e.target.setAttribute('src', fallbackImageUrl)
     }
     const hideBrokenImageHandler = e => {
       e.target.className = 'hidden'
     }
-
-    if (fallbackImageUrl) {
-      const testFallbackImage = new Image()
-      testFallbackImage.src = fallbackImageUrl
-      testFallbackImage.onload = () => {
-        images.forEach(img => {
-          img.addEventListener('error', loadFallbackImageHandler)
-        })
-      }
-      testFallbackImage.onerror = () => {
-        console.error('invalid fallbackImageUrl provided.', fallbackImageUrl)
-        images.forEach(img => {
-          img.addEventListener('error', hideBrokenImageHandler)
-        })
-      }
-    } else {
-      images.forEach(img => {
-        img.addEventListener('error', hideBrokenImageHandler)
-      })
-    }
+    images.forEach(img => {
+      const handler = fallbackImageUrl
+        ? loadFallbackImageHandler
+        : hideBrokenImageHandler
+      img.addEventListener('error', handler)
+    })
     return () => {
       images.forEach(img => {
         img.removeEventListener('error', hideBrokenImageHandler)
