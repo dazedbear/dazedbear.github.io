901ea218ca0454ef5280a1adb2935130fc299cdd
dazedbear <angus912584@gmail.com>
2021-06-13 20:30:10 +0800

    feat: add api route for notion list pagination

---
+++ src/pages/api/posts.ts
@@ -0,0 +1,139 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import createError from 'http-errors'
+import Ajv from 'ajv'
+import get from 'lodash/get'
+import set from 'lodash/set'
+import log from '../../libs/server/log'
+import { getNotionPostsFromTable, getNotionPreviewImages } from '../../libs/server/notion'
+import { notion } from '../../../site.config'
+
+const schema = {
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    index: {
+      type: 'string',
+      pattern: '^[0-9]+$',
+    },
+    pageName: {
+      enum: ['article', 'coding'], // align to site.config notion page names
+      type: 'string',
+    },
+    count: {
+      type: 'string',
+      pattern: '^[0-9]+$',
+    },
+  },
+  required: ['count', 'index', 'pageName'],
+}
+const ajv = new Ajv()
+const validate = ajv.compile(schema)
+
+const category = 'API route: /api/posts'
+const messageWithHeaders = (req, message = '') =>
+  `${message} \n${JSON.stringify(req.headers)}`
+const isSupportedMethod = req => {
+  const allowMethods = new Set(['GET'])
+  return allowMethods.has(req.method)
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    if (!isSupportedMethod(req)) {
+      log({
+        category,
+        message: messageWithHeaders(req, `unsupport method: ${req.method}`),
+        level: 'warn',
+      })
+      throw createError(400)
+    }
+    if (!validate(req.query)) {
+      log({
+        category,
+        message: messageWithHeaders(
+          req,
+          `invalid query params: ${JSON.stringify(req.query)}`
+        ),
+        level: 'warn',
+      })
+      throw createError(400)
+    }
+
+    const pageName = req.query?.pageName
+    const pageId = get(notion, ['pages', pageName, 'pageId'])
+    const collectionViewId = get(notion, [
+      'pages',
+      pageName,
+      'collectionViewId',
+    ])
+    const pageEnabled = get(notion, ['pages', pageName, 'enabled'])
+
+    if (!pageEnabled) {
+      log({
+        category,
+        message: messageWithHeaders(
+          req,
+          `posts data not found because this page is disabled \nquery params = ${JSON.stringify(
+            req.query
+          )}`
+        ),
+        level: 'warn',
+      })
+      throw createError(404)
+    }
+    if (!pageId || !collectionViewId) {
+      log({
+        category,
+        message: messageWithHeaders(
+          req,
+          `posts data not found because some required configs are missing \nquery params = ${JSON.stringify(
+            req.query
+          )}`
+        ),
+        level: 'warn',
+      })
+      throw createError(404)
+    }
+
+    const index: any = req.query?.index
+    const count: any = req.query?.count
+    const limit = Number.parseInt(index) + Number.parseInt(count)
+    if (isNaN(limit) || !Number.isInteger(limit)) {
+      log({
+        category,
+        message: messageWithHeaders(
+          req,
+          `invalid index or count, query params: ${JSON.stringify(req.query)}`
+        ),
+        level: 'warn',
+      })
+      throw createError(400)
+    }
+
+    // fetch notion post data
+    log({
+      category,
+      message: messageWithHeaders(
+        req,
+        `dumpaccess, query params: ${JSON.stringify(req.query)}`
+      ),
+    })
+    const data = await getNotionPostsFromTable({
+      pageId,
+      collectionViewId,
+      options: { limit },
+    })
+
+    if (get(notion, ['previeImages', 'enable'])) {
+      const previewImageMap = await getNotionPreviewImages(data.recordMap)
+      set(data, ['recordMap', 'preview_images'], previewImageMap)
+    }
+
+    res.status(200).json(data)
+  } catch (err) {
+    const statusCode = err.status || 500
+    res.status(statusCode).send(err.message)
+  }
+}
+
+export default handler
