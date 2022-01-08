2d3b33dc0ffe5eef6ffc3d178c842497a13ade64
dazedbear <angus912584@gmail.com>
2022-01-09 02:10:15 +0800

    feat: add page timeout to prevent lambda 504

--- src/pages/[pageName]/[pageSlug].tsx
+++ src/pages/[pageName]/[pageSlug].tsx
@@ -15,7 +15,7 @@ import {
 } from 'react-notion-x'
 
 import { logOption } from '../../../types'
-import { notion } from '../../../site.config'
+import { notion, pageProcessTimeout } from '../../../site.config'
 import { PAGE_TYPE_ARTICLE_SINGLE_PAGE } from '../../libs/constant'
 import {
   extractSinglePagePath,
@@ -32,6 +32,7 @@ import {
   fetchArticleStream,
   isValidPageSlug,
   isValidPageName,
+  executeFunctionWithTimeout,
 } from '../../libs/server/page'
 import {
   transformArticleStream,
@@ -49,72 +50,95 @@ import NotionPageFooter from '../../components/notion-page-footer'
 
 export const getServerSideProps: GetServerSideProps = wrapper.getServerSideProps(
   store => async ({ params: { pageName, pageSlug }, req, res }) => {
-    if (!isValidPageName(pageName) || !isValidPageSlug(pageSlug)) {
-      const options: logOption = {
-        category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
-        message: `invalid page | pageName: ${pageName} | pageSlug: ${pageSlug}`,
-        level: 'error',
-        req,
-      }
-      log(options)
-      return showCommonPage(req, res, 'notFound', pageName)
-    }
+    const props = await executeFunctionWithTimeout(
+      async () => {
+        if (!isValidPageName(pageName) || !isValidPageSlug(pageSlug)) {
+          const options: logOption = {
+            category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
+            message: `invalid page | pageName: ${pageName} | pageSlug: ${pageSlug}`,
+            level: 'error',
+            req,
+          }
+          log(options)
+          return showCommonPage(req, res, 'notFound', pageName)
+        }
 
-    const { pageId: articleId } = extractSinglePagePath(pageSlug)
+        const { pageId: articleId } = extractSinglePagePath(pageSlug)
 
-    try {
-      let articleStream = {}
-      const response = await fetchArticleStream({
-        req,
-        pageName,
-        category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
-      })
-      articleStream = await transformArticleStream(pageName, response)
-      articleStream = await transformArticleStreamPreviewImages(articleStream)
+        try {
+          let articleStream = {}
+          const response = await fetchArticleStream({
+            req,
+            pageName,
+            category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
+          })
+          articleStream = await transformArticleStream(pageName, response)
+          articleStream = await transformArticleStreamPreviewImages(
+            articleStream
+          )
 
-      // since getPage for collection view returns all pages with partial blocks, getPage target article then merge to articleStream to add missing blocks
-      const singleArticleResponse = await fetchArticleStream({
-        req,
-        pageId: articleId,
-        category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
-      })
-      let singleArticle = await transformSingleArticle(singleArticleResponse)
-      singleArticle = await transformArticleStreamPreviewImages(singleArticle)
-      articleStream = merge(articleStream, singleArticle)
+          // since getPage for collection view returns all pages with partial blocks, getPage target article then merge to articleStream to add missing blocks
+          const singleArticleResponse = await fetchArticleStream({
+            req,
+            pageId: articleId,
+            category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
+          })
+          let singleArticle = await transformSingleArticle(
+            singleArticleResponse
+          )
+          singleArticle = await transformArticleStreamPreviewImages(
+            singleArticle
+          )
+          articleStream = merge(articleStream, singleArticle)
 
-      const menuItems = transformMenuItems(pageName, articleStream)
-      const toc = transformTableOfContent(articleStream, articleId)
+          const menuItems = transformMenuItems(pageName, articleStream)
+          const toc = transformTableOfContent(articleStream, articleId)
 
-      // save SSR fetch stream article contents to redux store
-      const payload = transformStreamActionPayload(pageName, articleStream)
-      const action = updateStream(payload)
-      store.dispatch(action)
+          // save SSR fetch stream article contents to redux store
+          const payload = transformStreamActionPayload(pageName, articleStream)
+          const action = updateStream(payload)
+          store.dispatch(action)
 
-      const options: logOption = {
-        category: 'page',
-        message: `dumpaccess to /${pageName}/${pageSlug}`,
-        level: 'info',
-        req,
-      }
-      log(options)
-      return {
-        props: {
-          menuItems,
-          pageId: idToUuid(articleId),
-          pageName,
-          toc,
-        },
-      }
-    } catch (err) {
-      const options: logOption = {
-        category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
-        message: err,
-        level: 'error',
-        req,
-      }
-      log(options)
-      return showCommonPage(req, res, 'error', pageName)
-    }
+          const options: logOption = {
+            category: 'page',
+            message: `dumpaccess to /${pageName}/${pageSlug}`,
+            level: 'info',
+            req,
+          }
+          log(options)
+          return {
+            props: {
+              menuItems,
+              pageId: idToUuid(articleId),
+              pageName,
+              toc,
+            },
+          }
+        } catch (err) {
+          const options: logOption = {
+            category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
+            message: err,
+            level: 'error',
+            req,
+          }
+          log(options)
+          return showCommonPage(req, res, 'error', pageName)
+        }
+      },
+      pageProcessTimeout,
+      duration => {
+        const options: logOption = {
+          category: PAGE_TYPE_ARTICLE_SINGLE_PAGE,
+          message: `page processing timeout | duration: ${duration} ms`,
+          level: 'warn',
+          req,
+        }
+        log(options)
+        return showCommonPage(req, res, 'error', pageName)
+      },
+      PAGE_TYPE_ARTICLE_SINGLE_PAGE
+    )
+    return props
   }
 )
 
