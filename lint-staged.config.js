ae15cf494b74ca2224d9e8a7342a20dc73043a97
dazedbear <angus912584@gmail.com>
2022-08-20 14:59:25 +0800

    build: add styleline

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   '*': filenames => {
     const changedFiles = filenames.join(' ')
-    return ['npm run lint', 'npm run format', `git add ${changedFiles}`]
+    return [
+      'npm run lint:eslint',
+      'npm run lint:stylelint',
+      'npm run format',
+      `git add ${changedFiles}`,
+    ]
   },
 }
