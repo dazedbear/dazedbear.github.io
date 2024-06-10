660e4ccba1426a665a823e2e26a2c422cda036ce
dazedbear <angus912584@gmail.com>
2024-06-10 11:34:17 +0800

    refactor: remove unused redux slices and page types

--- src/libs/client/slices/index.ts
+++ src/libs/client/slices/index.ts
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux'
 import layout from './layout'
-import stream from './stream'
-import page from './page'
 
 const rootReducer = combineReducers({
   layout,
-  page,
-  stream,
 })
 
 export default rootReducer
